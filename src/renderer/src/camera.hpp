f3ad8bbdfda2fcbbb8a3de7943d3c564903b0c68
rytrix <106265277+rytrix@users.noreply.github.com>
2025-11-08 13:13:37 -0500

    fix: modernized camera

--- src/renderer/src/camera.hpp
+++ src/renderer/src/camera.hpp
@@ -7,68 +7,73 @@ namespace Renderer {
 
 class Camera {
 public:
-    enum class movement;
-    Camera(float fov, float near, float far, uint32_t screenWidth, uint32_t screenHeight, glm::vec3 pos);
-    void updateProj();
-    void updateView();
-    void updateAspect(float aspectRatio);
+    enum class Movement;
+    Camera(float fov, float near, float far, float aspect_ratio, glm::vec3 pos);
+    void update_proj();
+    void update_view();
+    void update_aspect(float aspect_ratio);
     void update();
-    void move(movement direction, float deltaTime);
-    void rotate(float xoffset, float yoffset);
-    void updateVectors();
-    void zoom(float yoffset);
-    void setFirstMouse();
+    void move(Movement direction, float delta_time);
+    void rotate(float x_offset, float y_offset);
+    void update_vectors();
+    void zoom(float y_offset);
+    void set_first_mouse();
 
-    glm::mat4 getProj() const;
-    glm::mat4 getView() const;
-    glm::mat4 getCombined() const;
-    glm::mat4 getInverseView() const;
-    glm::mat4 getInverseProj() const;
-    glm::mat4 getInverseProjView() const;
-    glm::vec3 getPos() const;
-    glm::vec3 getFront() const;
-    glm::vec3 getUp() const;
-    glm::vec3 getRight() const;
+    [[nodiscard]] glm::mat4 get_proj() const;
+    [[nodiscard]] glm::mat4 get_view() const;
+    [[nodiscard]] glm::mat4 get_combined() const;
+    [[nodiscard]] glm::mat4 get_inverse_view() const;
+    [[nodiscard]] glm::mat4 get_inverse_proj() const;
+    [[nodiscard]] glm::mat4 get_inverse_proj_view() const;
+    [[nodiscard]] glm::vec3 get_pos() const;
+    [[nodiscard]] glm::vec3 get_front() const;
+    [[nodiscard]] glm::vec3 get_up() const;
+    [[nodiscard]] glm::vec3 get_right() const;
 
-    float getNear() const;
-    float getFar() const;
-    float getAspect() const;
-    float getFov() const;
+    [[nodiscard]] float get_near() const;
+    [[nodiscard]] float get_far() const;
+    [[nodiscard]] float get_aspect() const;
+    [[nodiscard]] float get_fov() const;
 
-    enum class movement {
-        forward,
-        backward,
-        left,
-        right,
-        up,
-        down,
+    enum class Movement {
+        Forward,
+        Backward,
+        Left,
+        Right,
+        Up,
+        Down,
     };
 
-    void setSpeed(float speed);
+    static constexpr float calculate_aspect_ratio(float width, float height)
+    {
+        return width / height;
+    }
+
+    void set_speed(float speed);
 
 private:
     bool m_first_mouse = true;
 
-    float m_fov;
-    float m_near;
-    float m_far;
-    float m_aspect_ratio;
+    float m_fov {};
+    float m_near {};
+    float m_far {};
+    float m_aspect_ratio {};
 
-    float m_speed;
-    float m_sensitivity;
+    float m_speed {};
+    float m_sensitivity {};
 
-    float m_last_x;
-    float m_last_y;
-    float m_yaw;
-    float m_pitch;
+    float m_last_x {};
+    float m_last_y {};
+    float m_yaw {};
+    float m_pitch {};
 
-    glm::vec3 m_pos;
-    glm::vec3 m_front;
-    glm::vec3 m_up;
-    glm::vec3 m_right;
+    glm::vec3 m_pos {};
+    glm::vec3 m_front {};
+    glm::vec3 m_up {};
+    glm::vec3 m_right {};
 
-    glm::mat4 m_proj;
-    glm::mat4 m_view;
+    glm::mat4 m_proj {};
+    glm::mat4 m_view {};
 };
 
 } // namespace Renderer
