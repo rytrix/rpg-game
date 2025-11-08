ac92573f494ae4d1293cf70d80fb465eaa6727b7
ryan <rmw071102@gmail.com>
2025-11-08 12:38:41 -0500

    fix: modernize buffer

--- src/renderer/src/buffer.cpp
+++ src/renderer/src/buffer.cpp
@@ -3,29 +3,35 @@
 namespace Renderer {
 
 Buffer::Buffer()
+    : m_id(0)
 {
-    glCreateBuffers(1, &id);
+    glCreateBuffers(1, &m_id);
 }
 
 Buffer::~Buffer()
 {
-    glDeleteBuffers(1, &id);
+    glDeleteBuffers(1, &m_id);
 }
 
-void Buffer::bufferData(GLsizeiptr size, const void* data, GLenum usage)
+void Buffer::buffer_data(GLsizeiptr size, const void* data, GLenum usage)
 {
-    glNamedBufferData(id, size, data, usage);
+    glNamedBufferData(m_id, size, data, usage);
 }
 
 // GL_DYNAMIC_STORAGE_BIT, GL_MAP_READ_BIT GL_MAP_WRITE_BIT, GL_MAP_PERSISTENT_BIT, GL_MAP_COHERENT_BIT, and GL_CLIENT_STORAGE_BIT.
-void Buffer::bufferStorage(GLsizeiptr size, const void* data, GLbitfield flags)
+void Buffer::buffer_storage(GLsizeiptr size, const void* data, GLbitfield flags)
 {
-    glNamedBufferStorage(id, size, data, flags);
+    glNamedBufferStorage(m_id, size, data, flags);
 }
 
-void Buffer::bufferSubData(GLsizeiptr offset, GLsizeiptr size, const void* data)
+void Buffer::buffer_sub_data(GLsizeiptr offset, GLsizeiptr size, const void* data)
 {
-    glNamedBufferSubData(id, offset, size, data);
+    glNamedBufferSubData(m_id, offset, size, data);
+}
+
+[[nodiscard]] GLuint Buffer::get_id() const
+{
+    return m_id;
 }
 
 } // namespace Renderer
