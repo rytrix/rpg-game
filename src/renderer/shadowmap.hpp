f7996d7d6802d21c8d3086272df98cfabe20b396
rytrix <106265277+rytrix@users.noreply.github.com>
2025-12-19 14:53:49 -0500

    update: moved shadowmap shaders to shadowmap file, added blit_depth_buffer() function

--- src/renderer/shadowmap.hpp
+++ src/renderer/shadowmap.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "texture.hpp"
 #include "framebuffer.hpp"
+#include "texture.hpp"
 
 namespace Renderer {
 
@@ -16,6 +16,32 @@ public:
 
     i32 get_width();
     i32 get_height();
+
+    static consteval const char* get_vertex_shader()
+    {
+        return R"(
+            #version 460 core
+            layout (location = 0) in vec3 aPos;
+
+            uniform mat4 light_space_matrix;
+            uniform mat4 model;
+
+            void main()
+            {
+            gl_Position = light_space_matrix * model * vec4(aPos, 1.0);
+            }
+        )";
+    }
+
+    static consteval const char* get_frag_shader()
+    {
+        return R"(
+            #version 460 core
+            void main()
+            {
+            }
+        )";
+    }
 
 private:
     static constexpr i32 DEFAULT_SHADOW_WIDTH = 1024;
