2e0d62a007ac886c6002b50d25253f124e70fca4
rytrix <106265277+rytrix@users.noreply.github.com>
2026-03-25 04:08:10 -0400

    rename: RandomSamplingTexture member variables were not using m_ prefix

--- src/renderer/random_sampling_texture.cpp
+++ src/renderer/random_sampling_texture.cpp
@@ -36,10 +36,10 @@ RandomSamplingTexture RandomSamplingTexture::create(i32 window_size, i32 filter_
 }
 
 RandomSamplingTexture::RandomSamplingTexture(Handle handle, i32 window_size, i32 filter_size, i32 radius)
-    : radius(radius)
-    , handle(handle)
-    , window_size(window_size)
-    , filter_size(filter_size)
+    : m_radius(radius)
+    , m_handle(handle)
+    , m_window_size(window_size)
+    , m_filter_size(filter_size)
 {
 }
 
@@ -47,14 +47,14 @@ void RandomSamplingTexture::bind_uniforms(ShaderProgram& shader, const char* uni
 {
     Utils::String buffer;
 
-    Renderer::Texture* texture = cache->get(handle);
+    Renderer::Texture* texture = cache->get(m_handle);
     GLuint texture_unit = Renderer::Texture::get_texture_unit();
     texture->bind(texture_unit);
 
     shader.set_int(buffer.format("{}.texture", uniform_name).cstr(), static_cast<int>(texture_unit));
-    shader.set_int(buffer.format("{}.window_size", uniform_name).cstr(), window_size);
-    shader.set_int(buffer.format("{}.filter_size", uniform_name).cstr(), filter_size);
-    shader.set_int(buffer.format("{}.radius", uniform_name).cstr(), radius);
+    shader.set_int(buffer.format("{}.window_size", uniform_name).cstr(), m_window_size);
+    shader.set_int(buffer.format("{}.filter_size", uniform_name).cstr(), m_filter_size);
+    shader.set_int(buffer.format("{}.radius", uniform_name).cstr(), m_radius);
 }
 
 Handle RandomSamplingTexture::create_random_sampling_texture(i32 window_size, int filter_size, TextureCache* cache)
--- src/renderer/random_sampling_texture.hpp
+++ src/renderer/random_sampling_texture.hpp
@@ -9,21 +9,21 @@ class ShaderProgram;
 struct RandomSamplingTexture : public NoCopyNoMove {
     static RandomSamplingTexture create(i32 window_size, i32 filter_size, i32 radius, TextureCache* cache);
 
-    [[nodiscard]] Handle get_handle() const { return handle; }
-    [[nodiscard]] i32 get_window_size() const { return window_size; }
-    [[nodiscard]] i32 get_filter_size() const { return filter_size; }
+    [[nodiscard]] Handle get_handle() const { return m_handle; }
+    [[nodiscard]] i32 get_window_size() const { return m_window_size; }
+    [[nodiscard]] i32 get_filter_size() const { return m_filter_size; }
 
     void bind_uniforms(ShaderProgram& shader, const char* uniform_name, TextureCache* cache);
 
-    i32 radius;
+    i32 m_radius;
 
 private:
     RandomSamplingTexture(Handle handle, i32 window_size, i32 filter_size, i32 radius);
     static Handle create_random_sampling_texture(i32 window_size, i32 filter_size, TextureCache* cache);
 
-    Handle handle;
-    i32 window_size;
-    i32 filter_size;
+    Handle m_handle;
+    i32 m_window_size;
+    i32 m_filter_size;
 };
 
 } // namespace Renderer
